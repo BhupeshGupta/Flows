0b2bec8b38e9ba07cc95e9fcb775c402a73a0572
bhupeshg2 <bhupesh00gupta@gmail.com>
2015-03-03 23:46:33 +0530

    unhide posting date and time in Goods Receipt

--- flows/flows/report/sales_report/sales_report.js
+++ flows/flows/report/sales_report/sales_report.js
@@ -2,7 +2,20 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Sales Report"] = {
-	"filters": [
-
-	]
-}
+    "filters": [
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "width": "80",
+            "default": frappe.datetime.get_today()
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "width": "80",
+            "default": frappe.datetime.get_today()
+        }
+    ]
+};
