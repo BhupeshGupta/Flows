22268dbed459bf33600b389ee9905181763e5fec
bhupeshg2 <bhupesh00gupta@gmail.com>
2015-03-13 18:08:17 +0530

    report and import enabled for customer plant variables and customer landed rate add opening and closing to sale purchase report (flow invoice report)

--- flows/flows/report/flows_invoice_report/flows_invoice_report.py
+++ flows/flows/report/flows_invoice_report/flows_invoice_report.py
@@ -17,13 +17,13 @@ def execute(filters=None):
 			data.append([
 				customer,
 				item,
+			    qty_dict.opening,
 				qty_dict.i_requested,
 			    qty_dict.m_purchased,
 				qty_dict.i_issued,
 				qty_dict.m_delivered,
 			    qty_dict.m_sold,
-				qty_dict.i_to_issue,
-				qty_dict.m_to_deliver
+				qty_dict.closing,
 			])
 
 	return get_columns(), data
@@ -33,20 +33,20 @@ def get_columns():
 	return [
 		"Customer:Link/Customer:200",
 		"Item:Link/Item:100",
+	    "Opening:Float:150",
 		"Indent Placed::",
 		"Purchase:Float:",
 		"Invoices Issued:Float:150",
 		"Material Delivered:Float:150",
 		"Sale:Float:",
-		"Invoices To Issue:Float:150",
-		"Material To Deliver:Float:150"
+		"Closing:Float:150",
 	]
 
 
 def get_invoices(filters):
-	return frappe.db.sql(
+	rs = frappe.db.sql(
 		"""
-		select customer, item, qty
+		select posting_date, customer, item, qty
 		from `tabIndent Invoice`
 		where docstatus = 1
 		and posting_date <= '{to_date}'
@@ -55,11 +55,17 @@ def get_invoices(filters):
 	    debug=True
 	)
 
+	# TODO find bug and remove hack
+	for r in rs:
+		r.posting_date = frappe.db.convert_to_simple_type(r.posting_date)
+
+	return rs
+
 
 def get_indents_which_are_not_invoiced_yet(filters):
 	return frappe.db.sql(
 		"""
-		select indent_item.customer, indent_item.item, indent_item.qty
+		select indent.posting_date as posting_date, indent_item.customer, indent_item.item, indent_item.qty
 		from `tabIndent Item` indent_item,
 		`tabIndent` indent
 		where indent_item.parent = indent.name
@@ -79,9 +85,9 @@ def get_indents_which_are_not_invoiced_yet(filters):
 def get_goods_receipts(filters):
 	return frappe.db.sql(
 		"""
-		select customer, item_delivered as item, ifnull(delivered_quantity, 0) as qty
+		select posting_date, customer, item_delivered as item, ifnull(delivered_quantity, 0) as qty
 		from `tabGoods Receipt`
-		where item_delivered != ''
+		where item_delivered like 'FC%'
 		and docstatus = 1
 		and posting_date <= '{to_date}';""".format(**filters),
 		as_dict=1,
@@ -92,7 +98,7 @@ def get_goods_receipts(filters):
 def get_sale_purchase_entries(filters):
 	return frappe.db.sql(
 		"""
-		select item, qty, from_customer, to_customer
+		select posting_date, item, qty, from_customer, to_customer
 		from `tabCross Sale Purchase`
 		where docstatus != 2
 		and posting_date <= '{to_date}';""".format(**filters),
@@ -103,6 +109,8 @@ def get_sale_purchase_entries(filters):
 
 def get_data_map(filters):
 	default = {
+	"opening": 0,
+
 	"i_requested": 0,
 	"i_issued": 0,
 	"m_purchased": 0,
@@ -110,11 +118,11 @@ def get_data_map(filters):
 	"m_delivered": 0,
 	"m_sold": 0,
 
-	"i_to_issue": 0,
-	"m_to_deliver": 0
+	"closing": 0,
 	}
 
-	map = {}
+	current_map = {}
+	opening_map = {}
 
 	invoices = get_invoices(filters)
 	indents = get_indents_which_are_not_invoiced_yet(filters)
@@ -122,34 +130,65 @@ def get_data_map(filters):
 	csps = get_sale_purchase_entries(filters)
 
 	for i in indents:
-		map.setdefault(i.customer, {}).setdefault(i.item, frappe._dict(default))
-		qty_dict = map[i.customer][i.item]
+		active_map = opening_map if i.posting_date < filters['from_date'] else current_map
+		active_map.setdefault(i.customer, {}).setdefault(i.item, frappe._dict(default))
+		qty_dict = active_map[i.customer][i.item]
 		qty_dict.i_requested += flt(i.qty)
 
 	for i in invoices:
-		map.setdefault(i.customer, {}).setdefault(i.item, frappe._dict(default))
-		qty_dict = map[i.customer][i.item]
+		active_map = opening_map if i.posting_date < filters['from_date'] else current_map
+		active_map.setdefault(i.customer, {}).setdefault(i.item, frappe._dict(default))
+		qty_dict = active_map[i.customer][i.item]
 		qty_dict.i_issued += flt(i.qty)
 
 	for i in csps:
-		map.setdefault(i.from_customer, {}).setdefault(i.item, frappe._dict(default))
-		map.setdefault(i.to_customer, {}).setdefault(i.item, frappe._dict(default))
+		active_map = opening_map if i.posting_date < filters['from_date'] else current_map
+		active_map.setdefault(i.from_customer, {}).setdefault(i.item, frappe._dict(default))
+		active_map.setdefault(i.to_customer, {}).setdefault(i.item, frappe._dict(default))
 
-		qty_dict = map[i.from_customer][i.item]
+		qty_dict = active_map[i.from_customer][i.item]
 		qty_dict.m_sold += i.qty
 
-		qty_dict = map[i.to_customer][i.item]
+		qty_dict = active_map[i.to_customer][i.item]
 		qty_dict.m_purchased += i.qty
 
 	for i in gr:
-		map.setdefault(i.customer, {}).setdefault(i.item, frappe._dict(default))
-		qty_dict = map[i.customer][i.item]
+		active_map = opening_map if i.posting_date < filters['from_date'] else current_map
+		active_map.setdefault(i.customer, {}).setdefault(i.item, frappe._dict(default))
+		qty_dict = active_map[i.customer][i.item]
 		qty_dict.m_delivered += flt(i.qty)
 
-	for customer in sorted(map):
-		for item in sorted(map[customer]):
-			qty_dict = map[customer][item]
-			diff = qty_dict.i_requested + qty_dict.i_issued + qty_dict.m_purchased - qty_dict.m_delivered - qty_dict.m_sold
-			qty_dict['m_to_deliver' if diff > 0 else 'i_to_issue'] = abs(diff)
+	active_map = opening_map
+	for customer in sorted(active_map):
+		for item in sorted(active_map[customer]):
+			qty_dict = active_map[customer][item]
+			diff = qty_dict.opening + qty_dict.i_requested + qty_dict.i_issued + qty_dict.m_purchased - qty_dict.m_delivered - qty_dict.m_sold
+			qty_dict['closing'] = diff
 
-	return map
+
+	# Compute closing for opening map
+	compute_closing(opening_map)
+
+	# Copy over closing to opening of current map
+	for customer in sorted(opening_map):
+		for item in sorted(opening_map[customer]):
+			qty_dict_from_opening_map = opening_map[customer][item]
+			current_map.setdefault(customer, {}).setdefault(item, frappe._dict(default))
+			qty_dict_from_current_map = current_map[customer][item]
+			qty_dict_from_current_map.opening = qty_dict_from_opening_map.closing
+
+	# Compute closing op current map
+	compute_closing(current_map)
+
+	return current_map
+
+
+
+def compute_closing(active_map):
+	for customer in sorted(active_map):
+		for item in sorted(active_map[customer]):
+			qty_dict = active_map[customer][item]
+			diff = qty_dict.opening + qty_dict.i_requested + qty_dict.i_issued + qty_dict.m_purchased - qty_dict.m_delivered - qty_dict.m_sold
+			qty_dict['closing'] = diff
+
+	return active_map
