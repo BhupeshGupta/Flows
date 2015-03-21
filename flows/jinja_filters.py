89a8a3ebdbbfb746fb61a41bff005a4959153a90
bhupeshg2 <bhupesh00gupta@gmail.com>
2015-03-21 15:44:36 +0530

    change single quotes to double in jinja filters

--- flows/jinja_filters.py
+++ flows/jinja_filters.py
@@ -6,7 +6,7 @@ import frappe
 def indent_refill_qty(indent_items):
 	sum = 0
 	for indent_item in indent_items:
-		if indent_item.load_type == 'Refill':
+		if indent_item.load_type == "Refill":
 			sum += indent_item.qty
 
 	return sum
@@ -15,7 +15,7 @@ def indent_refill_qty(indent_items):
 def indent_oneway_qty(indent_items):
 	sum = 0
 	for indent_item in indent_items:
-		if indent_item.load_type == 'Oneway':
+		if indent_item.load_type == "Oneway":
 			sum += indent_item.qty
 
 	return sum
@@ -24,8 +24,8 @@ def indent_oneway_qty(indent_items):
 def compute_erv_for_refill_in_indent(indent_items):
 	map = {}
 	for indent_item in indent_items:
-		if indent_item.load_type == 'Refill':
-			key = indent_item.item.replace('FC', '') + ' Kg'
+		if indent_item.load_type == "Refill":
+			key = indent_item.item.replace("FC", "") + " Kg"
 			map.setdefault(key, 0)
 			map[key] += indent_item.qty
 
@@ -33,7 +33,7 @@ def compute_erv_for_refill_in_indent(indent_items):
 	for values in map.values():
 		safety_caps += values
 	if safety_caps > 0:
-		map['Safety Caps'] = safety_caps
+		map["Safety Caps"] = safety_caps
 
 	return OrderedDict(sorted(map.items()))
 
@@ -42,13 +42,13 @@ def get_contract_number(customer, date, plant):
 	rs = frappe.db.sql("""
     SELECT contract_number
     FROM `tabCustomer Plant Variables`
-    WHERE customer = '{}'
-    AND with_effect_from < '{}'
-    AND plant = '{}'
+    WHERE customer = "{}"
+    AND with_effect_from < "{}"
+    AND plant = "{}"
     ORDER BY with_effect_from DESC limit 1;
     """.format(customer, date, plant))
 
-	return rs[0][0] if rs else ''
+	return rs[0][0] if rs else ""
 
 
 def get_registration_code(customer, vendor):
@@ -63,15 +63,15 @@ def get_registration_code(customer, vendor):
 
 	vendor = vendor.lower()
 
-	if vendor == 'hpc':
-		key = 'hpcl_erp_number'
-	elif vendor == 'bpc':
-		key = 'bpcl_sap_code'
-	elif vendor == 'ioc':
-		key = 'iocl_sap_code'
+	if vendor == "hpc":
+		key = "hpcl_erp_number"
+	elif vendor == "bpc":
+		key = "bpcl_sap_code"
+	elif vendor == "ioc":
+		key = "iocl_sap_code"
 
 	val = get_customer_field(customer, key)
-	return val if val else ''
+	return val if val else ""
 
 
 def get_customer_tin_number(customer):
@@ -79,7 +79,7 @@ def get_customer_tin_number(customer):
 	customer: customer to get code for
 	vendor: vendor to get registration code for (HPCL, BPLC, IOCL)
 	"""
-	return get_customer_field(customer, 'tin_number')
+	return get_customer_field(customer, "tin_number")
 
 
 def get_customer_field(customer_name, field):
@@ -90,27 +90,27 @@ def get_customer_field(customer_name, field):
     """.format(field, customer_name)
 	)
 
-	return rs[0][0] if rs else ''
+	return rs[0][0] if rs else ""
 
 
 def get_cenvat_status(customer_name, date, plant):
 	rs = frappe.db.sql("""
     SELECT {key}
     FROM `tabCustomer Plant Variables`
-    WHERE customer = '{customer}'
-    AND with_effect_from < '{date}'
-    AND plant = '{plant}'
+    WHERE customer = "{customer}"
+    AND with_effect_from < "{date}"
+    AND plant = "{plant}"
     ORDER BY with_effect_from DESC limit 1;
-    """.format(key='cenvat', customer=customer_name, date=date, plant=plant)
+    """.format(key="cenvat", customer=customer_name, date=date, plant=plant)
 	)
 
 	if not rs:
-		return ''
+		return ""
 
-	return 'MODVAT' if rs[0][0] == 1 else 'NONMODVAT'
+	return "MODVAT" if rs[0][0] == 1 else "NONMODVAT"
 
 
 def get_address_display(address_of, address_type):
 	from erpnext.utilities.doctype.address.address import get_address_display as gda
 
-	return gda('{}-{}'.format(address_of.strip(), address_type))
+	return gda("{}-{}".format(address_of.strip(), address_type))
