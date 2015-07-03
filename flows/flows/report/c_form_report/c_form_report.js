6ecedec591765bce20bf61bf476cde17c1a48433
bhupeshg2 <bhupesh00gupta@gmail.com>
2015-07-03 16:43:49 +0530

    Add C Form Report [Letter]

--- flows/flows/report/c_form_report/c_form_report.js
+++ flows/flows/report/c_form_report/c_form_report.js
@@ -3,6 +3,40 @@
 
 frappe.query_reports["C Form Report"] = {
     "filters": [
-
+	    {
+			"fieldname":"customer",
+			"label":__("Customer"),
+			"fieldtype":"Link",
+			"options":"Customer",
+			"reqd":1
+		},
+	    {
+			"fieldname":"from_date",
+			"label":__("From Date"),
+			"fieldtype":"Date",
+			"default":frappe.datetime.month_start(),
+			"reqd":1
+		},
+		{
+			"fieldname":"to_date",
+			"label":__("To Date"),
+			"fieldtype":"Date",
+			"default":frappe.datetime.month_end(),
+			"reqd":1
+		},
+		{
+			"fieldname":"supplier",
+			"label":__("Supplier"),
+			"fieldtype":"Link",
+			"options":"Supplier",
+			"reqd":1,
+			"get_query": function() {
+				return {
+					"filters": [
+						['Supplier', 'supplier_type', '=', 'OMC STATE'],
+					]
+				}
+			}
+		}
     ]
-}
+};
--- flows/flows/report/c_form_report/c_form_report.py
+++ flows/flows/report/c_form_report/c_form_report.py
@@ -6,9 +6,32 @@ import frappe
 
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+	data = list(get_data(filters))
+	data.append(['', 'Total Amount', '', sum([x[3] for x in data]), sum([x[4] for x in data])])
+	return get_columns(filters), data
 
 
-def get_data():
-	frappe.db.sql("Select customer, sum(actual_amount) from `tabIndent Invoice`;")
+def get_columns(filters):
+	return [
+		"Date:Date:95",
+		"Invoice No:Data:95",
+		"No Of Cylinders:Int:100",
+		"Amount:Currency:100",
+		"Amount With Tax:Currency:100",
+		"Supplier::",
+		"Tin No.::"
+	]
+
+
+def get_data(filters):
+	tin_no = frappe.db.sql("SELECT tin_number FROM `tabSupplier` WHERE name = '{supplier}'".format(**filters))[0][0]
+	return frappe.db.sql("""
+		SELECT i.transaction_date, i.invoice_number, i.qty, i.actual_amount * 0.98 AS actual_amount,
+		i.actual_amount AS amount_with_tax, s.name, s.tin_number
+		FROM `tabIndent Invoice` i, `tabSupplier` s
+		WHERE i.docstatus=1
+		AND i.supplier = s.name
+		AND s.tin_number = "{tin_no}"
+		AND i.customer = "{customer}"
+		AND i.transaction_date BETWEEN "{from_date}" AND "{to_date}";
+		""".format(tin_no=tin_no, **filters))
