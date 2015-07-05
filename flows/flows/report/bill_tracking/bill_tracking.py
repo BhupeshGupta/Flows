dd212e5ce9fc06a5ccabcb52c637f19429fe6380
bhupeshg2 <bhupesh00gupta@gmail.com>
2015-07-06 00:11:39 +0530

    Bill tracking enhancement

--- flows/flows/report/bill_tracking/bill_tracking.py
+++ flows/flows/report/bill_tracking/bill_tracking.py
@@ -22,13 +22,66 @@ def get_columns(filters):
 
 
 def get_date(filters):
+	# return frappe.db.sql("""
+	# SELECT indent.posting_date, indent.name, indent_item.customer, indent_invoice.transaction_date,
+	# indent_invoice.invoice_number, ifnull(indent_invoice.invoice_receive_date, '')
+	# FROM `tabIndent Item` indent_item, `tabIndent` indent, `tabIndent Invoice` indent_invoice
+	# WHERE indent_item.parent = indent.name
+	# AND indent_item.name = indent_invoice.indent_item
+	# AND indent_invoice.docstatus != 2
+	# AND indent.posting_date BETWEEN '{from_date}' AND '{to_date}'
+	# ORDER BY indent.posting_date DESC;
+	# """.format(**filters))
+
+	invoice_map = {}
+	rows = []
+
+	indent_items = get_indents_items(filters)
+	invoices = get_invoices(filters)
+
+	for i in invoices:
+		invoice_map[i.indent_item] = i
+
+	for i in indent_items:
+		row = [
+			i.posting_date,
+			i.indent_name,
+			i.customer
+		]
+
+		if i.name in invoice_map:
+			invoice = invoice_map[i.name]
+			row.extend([
+				invoice.transaction_date,
+				invoice.invoice_number,
+				invoice.invoice_receive_date
+			])
+		else:
+			row.extend([''] * 3)
+
+		rows.append(row)
+
+	return rows
+
+
+def get_indents_items(filters):
 	return frappe.db.sql("""
-	SELECT indent.posting_date, indent.name, indent_item.customer, indent_invoice.transaction_date,
-	indent_invoice.invoice_number, ifnull(indent_invoice.invoice_receive_date, '')
-	FROM `tabIndent Item` indent_item, `tabIndent` indent, `tabIndent Invoice` indent_invoice
+	SELECT indent_item.name, indent.posting_date, indent.name AS indent_name, indent_item.customer
+	FROM `tabIndent Item` indent_item, `tabIndent` indent
 	WHERE indent_item.parent = indent.name
-	AND indent_item.name = indent_invoice.indent_item
-	AND indent_invoice.docstatus != 2
 	AND indent.posting_date BETWEEN '{from_date}' AND '{to_date}'
-	ORDER BY indent.posting_date desc;
-	""".format(**filters))
+	ORDER BY indent.posting_date DESC, indent_item.customer
+	""".format(**filters), as_dict=True)
+
+
+def get_invoices(filters):
+	return frappe.db.sql("""
+	SELECT * FROM `tabIndent Invoice`
+	WHERE indent_item IN (
+		SELECT indent_item.name
+		FROM `tabIndent Item` indent_item, `tabIndent` indent
+		WHERE indent_item.parent = indent.name
+		AND indent.posting_date BETWEEN '{from_date}' AND '{to_date}'
+		ORDER BY indent.posting_date DESC, indent_item.customer
+	)
+	""".format(**filters), as_dict=True)
