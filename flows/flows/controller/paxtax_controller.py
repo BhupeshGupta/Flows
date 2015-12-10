471bf48522d824df9a6b4969435c4397bbc423dd
Jaskaran Deol <karan@Jaskarans-MacBook-Pro.local>
2015-12-10 18:29:07 +0530

    pextax init

--- flows/flows/controller/paxtax_controller.py
+++ flows/flows/controller/paxtax_controller.py
@@ -1,28 +1,38 @@
 from requests import Session
 import shutil
-
+from subprocess import call
 
 def login(c):
 	request = c.get("https://www.pextax.com/PEXWAR/appmanager/pexportal/PunjabExcise?_nfpb=true&_nfls=false&_pageLabel=login")
-
-	print request.text
 
 	response = c.get("https://www.pextax.com/PEXWAR/stickyImg", stream=True)
 	with open('/tmp/pexcap.png', 'wb') as out_file:
 		shutil.copyfileobj(response.raw, out_file)
 	del response
 
-	# payload = {
-	# 	'username': 'ICC00022873',
-	# 	'password': '6pN@COk7m',
-	# }
+	call(['/usr/bin/tesseract', '/tmp/pexcap.png', '/tmp/pexcap'])
 
-	# response = c.post(
-	# 	"https://www.pextax.com/PEXWAR/appmanager/pexportal/PunjabExcise?_nfpb=true&_windowLabel=HeaderController_2",
-	# 	data=payload).text
-	#
-	# print(response)
+	ocr_file = open('/tmp/pexcap.txt', mode='r')
+	ocr_value = ocr_file.read()
+	ocr_file.close()
+
+	if ocr_value:
+
+		payload = {
+			'username': 'ICC00022873',
+			'password': '6pN@COk7m',
+			'answer': ocr_value
+		}
+
+		response = c.post(
+			"https://www.pextax.com/PEXWAR/appmanager/pexportal/PunjabExcise?_nfpb=true&_windowLabel=HeaderController_2",
+			data=payload
+		)
+
+		return 'userdetail' in response.text
+
+
 
 
 session = Session()
-login(session)
+print login(session)
