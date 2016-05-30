4d8ddf25a26075568eb897498cdca07d28705e7d
Ravi singh <rvsingh011@gmail.com>
2016-05-30 13:49:21 +0530

    [update]  erp server alfresco

--- flows/flows/controller/ephesoft_integration.py
+++ flows/flows/controller/ephesoft_integration.py
@@ -1,7 +1,14 @@
 import json
-
+from frappe.utils.file_manager import upload
 import frappe
+import requests
+from frappe.utils import cint
+from flows.stdlogger import root
 
+
+
+if not frappe.conf.document_queue_server:
+	raise Exception('document_queue_server not found in config')
 
 @frappe.whitelist(allow_guest=True)
 def get_meta(doc):
@@ -42,3 +49,48 @@ def get_meta(doc):
 		)
 
 	return json.dumps(return_doc)
+
+
+def create_docs_in_review_server(doc, method=None, *args, **kwargs):
+	docs = []
+
+	sales_invoice_amended_from = frappe.db.get_value("Sales Invoice", doc.transportation_invoice, 'amended_from')
+
+	# Normalizations
+	doc.transportation_invoice_number = '-'.join(doc.transportation_invoice.split('-')[:-1]) if (sales_invoice_amended_from and sales_invoice_amended_from.strip()) else doc.transportation_invoice
+
+	# Material Bill / Indent Invoice
+	docs.append({'cno': doc.transportation_invoice_number, 'doctype': doc.doctype, 'date': doc.transaction_date})
+
+	# Consignment note
+	docs.append({'cno': doc.transportation_invoice_number, 'doctype': "Consignment Note", 'date': doc.posting_date})
+
+	if (doc.sales_tax == 'CST'):
+		docs.append({'cno': doc.transportation_invoice_number, 'doctype': "VAT Form XII", 'date': doc.transaction_date})
+
+	if ('hpcl' in doc.supplier.lower() and cint(doc.cenvat) == 1):
+		docs.append({'cno': doc.transportation_invoice_number, 'doctype': "Excise Invoice", 'date': doc.transaction_date})
+
+	for sails_doc in docs:
+		data = requests.post('{}/currentstat/'.format(frappe.conf.document_queue_server), sails_doc)
+		root.debug(data)
+
+
+@frappe.whitelist()
+def get_user():
+	return json.dumps({'user': frappe.session.user})
+
+
+# @frappe.whitelist()
+# def attach_doc():
+# 	frappe.form_dict.docname = frappe.db.sql("""
+# 		select name
+# 		from `tab{doctype}`
+# 		where (
+# 			name = '{docname}'
+# 			or name like '{docname}-%'
+# 		)
+# 		and docstatus = 1
+# 	""".format(**frappe.form_dict), as_dict=True)[0].name
+#
+# 	upload()
