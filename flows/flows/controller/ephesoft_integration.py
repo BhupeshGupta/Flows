fba4ce5d6038d228e91008aaa0cec85730a50d3f
ERPNext <erpnext@erpnext-vm.erpnext-vm>
2016-10-01 16:25:07 +0530

    [Update] Episoft Controller - Added Search by Indent Invoice

--- flows/flows/controller/ephesoft_integration.py
+++ flows/flows/controller/ephesoft_integration.py
@@ -9,43 +9,54 @@ import frappe
 if not frappe.conf.document_queue_server:
 	raise Exception('document_queue_server not found in config')
 
+
+def get_sales_invoice_id_from_doc_id(id):
+	return frappe.db.sql(
+		"""
+		select transportation_invoice
+		from `tabIndent Invoice`
+		where (
+			name = '{0}' or
+			name like '{0}-%'
+		)
+			and docstatus = 1
+		""".format(id)
+	)[0][0]
+
+
+
 @frappe.whitelist(allow_guest=True)
 def get_meta(doc):
 	doc = json.loads(doc)
 
 	query = {
-	'doctype': 'Sales Invoice' if doc['type'] in ('Consignment Note',) else '',
-	'name': doc['id']
+		'name': doc['id'] if doc['type'] in ('Consignment Note',) else get_sales_invoice_id_from_doc_id(doc['id'])
 	}
 
-	return_doc = {}
-	if doc['type'] == 'Consignment Note':
-		return_doc.update(
-			frappe.db.sql("""
-			select invoice_number as `Bill Number`, transaction_date as `Bill Date`, item as `Bill Item`,
-			qty as `Bill Quantity`, actual_amount as `Bill Amount`, supplier as `Bill Supplier`,
-			vehicle as `Vehicle`, customer as `Customer`
-			from `tabIndent Invoice`
-			where (
-				transportation_invoice = '{name}'
-				or transportation_invoice like '{name}-%'
-			)
-			and docstatus = 1
-			""".format(**query), as_dict=True)[0]
+	return_doc = frappe.db.sql("""
+		select invoice_number as `Bill Number`, transaction_date as `Bill Date`, item as `Bill Item`,
+		qty as `Bill Quantity`, actual_amount as `Bill Amount`, supplier as `Bill Supplier`,
+		vehicle as `Vehicle`, customer as `Customer`
+		from `tabIndent Invoice`
+		where (
+			transportation_invoice = '{name}'
+			or transportation_invoice like '{name}-%'
 		)
+		and docstatus = 1
+	""".format(**query), as_dict=True)[0]
 
-		return_doc.update(
-			frappe.db.sql("""
+	return_doc.update(
+		frappe.db.sql("""
 			select name as `Consignment Name`, posting_date as `Consignment Date`,
 			grand_total_export as `Consignment Amount`, company as `Consignment Company`,
 			amended_from as `$amended_from`
-			from `tab{doctype}`
+			from `tabSales Invoice`
 			where (
 			name = '{name}'
 			or name like '{name}-%'
 			) and docstatus != 2
-			""".format(**query), as_dict=True)[0]
-		)
+		""".format(**query), as_dict=True)[0]
+	)
 
 	return json.dumps(return_doc)
 
