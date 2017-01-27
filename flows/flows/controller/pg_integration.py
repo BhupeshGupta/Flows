6df5c8d3c0b7d23b44f3b23a94704bc37eba12f9
bhupeshg2 <bhupesh00gupta@gmail.com>
2017-01-27 15:08:46 +0530

    [Update] PG Api

--- flows/flows/controller/pg_integration.py
+++ flows/flows/controller/pg_integration.py
@@ -4,4 +4,4 @@ import frappe
 
 @frappe.whitelist(allow_guest=True)
 def get_customers(phone):
-	return json.dumps(frappe.db.get_values("Contact", filters={"phone":phone}, fieldname="customer", as_dict=False))
+	return json.dumps(frappe.db.get_values("Contact", filters={"phone":phone}, fieldname="customer", as_dict=True))
