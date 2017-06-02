494934e086818c1a0addc51a56efdec3f698b5d3
ERPNext Deployment <bhupesh00gupta@gmail.com>
2017-06-02 19:42:41 +0530

    [Update] PG Integration Updated to support portal

--- flows/flows/controller/pg_integration.py
+++ flows/flows/controller/pg_integration.py
@@ -4,4 +4,28 @@ import frappe
 
 @frappe.whitelist(allow_guest=True)
 def get_customers(phone):
-	return json.dumps(frappe.db.get_values("Contact", filters={"phone":phone}, fieldname="customer", as_dict=True))
+	customer_list = frappe.db.get_values("Contact", filters={"phone": phone}, fieldname="customer", as_dict=True)
+
+	for cust in customer_list:
+		customer = cust.customer
+		pan_number = frappe.db.get_values("Customer", filters={"name": customer}, fieldname="pan_number", as_dict=True)[0]
+		if pan_number:
+			cust.update(pan_number)
+
+		address = frappe.db.get_values(
+			"Address",
+			filters={"customer": customer, 'is_primary_address': 1},
+			fieldname=[
+				'address_title',
+				'address_line1',
+				'address_line2',
+				'city',
+				'state',
+				'pincode'
+			],
+			as_dict=True
+		)
+
+		cust.update(address[0] if address else {'address_not_found': True})
+
+	return json.dumps(customer_list)
