dea8223faff959f0c32d8b2e67ddc14dabbd3a2d
bhupeshg2 <bhupesh00gupta@gmail.com>
2017-08-02 16:28:02 +0530

    [Updtae]

--- flows/flows/custom_validation.py
+++ flows/flows/custom_validation.py
@@ -111,10 +111,13 @@ def customer_onload(doc, method=None, *args, **kwargs):
 def validate_gst_number(doc, method=None, *args, **kwargs):
 	address = doc.customer_address
 	if address:
-		gst_number = frappe.db.get_value("Address", address, 'gst_number')
+		gst_number, gst_status = frappe.db.get_value("Address", address, ['gst_number', 'gst_status'])
 		if gst_number and gst_number.strip():
 			# if gst_number[:2] == '03':
 			return
 
-			frappe.throw("Out of state GST billing not enabled yet.".format(doc.customer))
-	frappe.throw("GST Number not found for customer {}. Can not raise invoice.".format(doc.customer))
+		if gst_status == 'Unregistered':
+			return
+
+		frappe.throw("Out of state GST billing not enabled yet.".format(doc.customer))
+	frappe.throw("Billing Address not found for customer {}. Can not raise invoice.".format(doc.customer))
